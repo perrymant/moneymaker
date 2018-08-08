76084218418f90f4fc155c2a65234c06d69779e5
Roger Guldbrandsen <roger@kinbiko.com>
2018-08-08 23:03:35 +0100

    Simplify tests without losing confidence

---
+++ src/io/github/perrymant/moneymaker/BalanceTest.java
@@ -0,0 +1,14 @@
+package io.github.perrymant.moneymaker;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BalanceTest {
+
+    @Test
+    public void startBalanceIsZero() {
+        assertEquals(0, new Balance().get());
+    }
+
+}
