9991cfe7bc46bdb7e2f7c8f6252b67236e299cdb
Thomas Perryman <tcw.perryman@gmail.com>
2018-09-11 20:56:00 +0100

    removed throw statement from FileIOTest

--- src/test/java/io/github/perrymant/moneymaker/FileIOTest.java
+++ src/test/java/io/github/perrymant/moneymaker/FileIOTest.java
@@ -2,14 +2,12 @@ package io.github.perrymant.moneymaker;
 
 import org.junit.Test;
 
-import java.io.IOException;
-
 import static org.junit.Assert.assertEquals;
 
 public class FileIOTest {
 
     @Test
-    public void canReadData() throws IOException {
+    public void canReadData()  {
         FileIO fileIO = new FileIO();
         String fileName = "src/test/resources/dump/file_with_data";
         String expected = "2018/01/01, CREDIT, 200, 200, Got paid\n2018/01/02, DEBIT, 50, 150, Paid bill\n";
@@ -18,7 +16,7 @@ public class FileIOTest {
     }
 
     @Test
-    public void canAppendData() throws IOException {
+    public void canAppendData() {
         FileIO fileIO = new FileIO();
         String fileName = "src/test/resources/dump/file_with_data_append_test";
         String dataToWrite = "2019/03/03, CREDIT, 200, 200, Got paid\n2019/03/04, DEBIT, 50, 150, Paid bill\n";
