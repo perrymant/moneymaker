51c3e8a5b0e648b4ae20ba488ee9089383f7baf6
Thomas Perryman <tcw.perryman@gmail.com>
2018-09-12 20:34:20 +0100

    delete file before running test

--- src/test/java/io/github/perrymant/moneymaker/FileIOTest.java
+++ src/test/java/io/github/perrymant/moneymaker/FileIOTest.java
@@ -1,23 +1,15 @@
 package io.github.perrymant.moneymaker;
 
 import org.apache.commons.io.FileUtils;
-import org.junit.Before;
 import org.junit.Test;
-
-import java.io.File;
 
 import static org.junit.Assert.assertEquals;
 
 public class FileIOTest {
 
-    @Before
-    public void setUp() throws Exception {
-        FileUtils.cleanDirectory(new File("src/test/resources/dump"));
-    }
-
     @Test
     public void canAppendData() {
-        String fileName = "src/test/resources/dump/file_with_data_append_test";
+        String fileName = FileUtils.getTempDirectoryPath() + "file_with_data_append_test";
         String dataToWrite = "2019/03/03, CREDIT, 200, 200, Got paid\n2019/03/04, DEBIT, 50, 150, Paid bill\n";
         String dataToAppend = "2018/01/01, CREDIT, 200, 200, Got paid\n2018/01/02, DEBIT, 50, 150, Paid bill\n";
         FileIO fileIO = new FileIO();
