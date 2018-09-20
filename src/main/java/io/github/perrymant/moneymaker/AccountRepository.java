35691dbd8b377b2a2c2ed7bb305bf172a25293a3
Thomas Perryman <tcw.perryman@gmail.com>
2018-09-20 20:59:19 +0100

    PR issues addressed

--- src/main/java/io/github/perrymant/moneymaker/AccountRepository.java
+++ src/main/java/io/github/perrymant/moneymaker/AccountRepository.java
@@ -1,20 +1,18 @@
 package io.github.perrymant.moneymaker;
 
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
 
-
 class AccountRepository {
+    private ObjectMapper mapper = new ObjectMapper();
 
     Account read(String fileName) {
-        ObjectMapper mapper = new ObjectMapper();
         try {
             return mapper.readValue(new File(fileName), Account.class);
         } catch (IOException e) {
-            throw new FileIOException("FileIOException: couldn't read",e);
+            throw new FileIOException("FileIOException: couldn't read", e);
         }
     }
 
@@ -23,15 +21,10 @@ class AccountRepository {
         try {
             mapper.writeValue(new File(fileName), data);
         } catch (IOException e) {
-            throw new FileIOException("FileIOException: couldn't write",e);
+            throw new FileIOException("FileIOException: couldn't write", e);
         }
     }
 
-    private class FileIOException extends RuntimeException {
-        FileIOException(String message, IOException e) {
-            super(message, e);
-        }
-    }
 }
 
 
