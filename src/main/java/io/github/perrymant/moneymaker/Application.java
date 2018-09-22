8dedfbb53b591c9cae80e68cd526cbe46af7942c
Thomas Perryman <tcw.perryman@gmail.com>
2018-09-22 15:44:33 +0100

    simplified main & application

--- src/main/java/io/github/perrymant/moneymaker/Application.java
+++ src/main/java/io/github/perrymant/moneymaker/Application.java
@@ -4,8 +4,6 @@ class Application {
     private final Budget budget = new Budget(new Balance(), new TransactionMaker().getTransactions());
 
     void start(String[] args) {
-        if (args[0].equals("-log")) {
-            new Logger().info(budget.report());
-        }
+        new Logger().info(budget.report());
     }
 }
