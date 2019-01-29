08a2ac1602ba6c3fbfadc5885c4d42cd9b218f0f
Thomas Perryman <tcw.perryman@gmail.com>
2019-01-29 21:37:34 +0000

    passing test assertJSONResponse

--- src/test/java/io/github/perrymant/moneymaker/HitRestEndpointTest.java
+++ src/test/java/io/github/perrymant/moneymaker/HitRestEndpointTest.java
@@ -3,22 +3,49 @@ package io.github.perrymant.moneymaker;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.Assert.assertNotNull;
-
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class HitRestEndpointTest {
 
     @Autowired
-    private ReportController reportController;
+    private
+    MockMvc mvc;
 
     @Test
-    public void testEndpoint() {
-        assertNotNull(reportController);
+    public void assertJSONResponse() {
+        String targetJSON = "" +
+                "{\n" +
+                "  \"report\":[\n" +
+                "    {\n" +
+                "      \"date\":\"2018-01-01\",\n" +
+                "      \"amount\":125,\n" +
+                "      \"description\":\"Got paid\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"date\":\"2018-01-02\",\n" +
+                "      \"amount\":-72,\n" +
+                "      \"description\":\"Paid bill\"\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
+        try {
+            mvc.perform(MockMvcRequestBuilders.get("/report"))
+                    .andExpect(status().isOk())
+                    .andExpect(content().string(equalTo(targetJSON)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
