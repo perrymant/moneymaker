dbffc30c7fec84c6ecc73e7f6f6611d99981eef7
Thomas Perryman <tcw.perryman@gmail.com>
2019-01-30 17:30:34 +0000

    can search response body

--- src/test/java/io/github/perrymant/moneymaker/HitRestEndpointTest.java
+++ src/test/java/io/github/perrymant/moneymaker/HitRestEndpointTest.java
@@ -1,17 +1,20 @@
 package io.github.perrymant.moneymaker;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.Matchers.contains;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -19,33 +22,23 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class HitRestEndpointTest {
 
     @Autowired
-    private
-    MockMvc mvc;
+    private MockMvc mvc;
+
+    private static final String BASE_URL = "/report";
+
+    @Ignore
+    @Test
+    public void assertJSONResponse1() throws Exception {
+        mvc.perform(get(BASE_URL).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.report", contains(125)))
+                .andReturn();
+    }
 
     @Test
-    public void assertJSONResponse() {
-        String targetJSON = "" +
-                "{\n" +
-                "  \"report\":[\n" +
-                "    {\n" +
-                "      \"date\":\"2018-01-01\",\n" +
-                "      \"amount\":125,\n" +
-                "      \"description\":\"Got paid\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"date\":\"2018-01-02\",\n" +
-                "      \"amount\":-72,\n" +
-                "      \"description\":\"Paid bill\"\n" +
-                "    }\n" +
-                "  ]\n" +
-                "}";
-        try {
-            mvc.perform(MockMvcRequestBuilders.get("/report"))
-                    .andExpect(status().isOk())
-                    .andExpect(content().string(equalTo(targetJSON)));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void assertJSONResponse2() throws Exception {
+        this.mvc.perform(get(BASE_URL)).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Got paid")));
     }
 
 }
